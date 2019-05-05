5140ad9c94f581d5c86717e969c1d9b037ff1ea0
Pansan98 <edo.fjsjm@icloud.com>
2019-05-05 15:51:38 +0900

    edit ajax

--- master-custom/admin/app/ajax/Capture/ajax.php
+++ master-custom/admin/app/ajax/Capture/ajax.php
@@ -1,13 +1,14 @@
 <?php
 
-use src\Mod\Controller\Capture\CaptureController as controller;
+use src\Mod\Controller\Base\BaseController as controller;
 
-if(isset($_POST['url'])) {
-    include_once dirname(__FILE__).'/../../../master-custom/bootstrap.php';
-    $CaptureController = new controller();
-    $CaptureController->setActionName('Capture')->setSsUrl($_POST['url']);
-    if($CaptureController->setCapture()->getCaptureShot()) {
-        $CaptureController->setRunSaves()->isRunSaves();
+if(isset($_POST['capture_url'])) {
+    include_once dirname(__FILE__).'/../../../../bootstrap.php';
+    $baseController = new controller();
+    $actionController = $baseController->setActionName('Capture')->getController();
+    $actionController->setSsUrl($actionController->setHelper()->getPostQueryParam('capture_url'));
+    if($actionController->setCapture()->getCaptureShot()) {
+        $actionController->setRunSaves()->isRunSaves();
     }
 }
 ?>
--- master-custom/admin/src/App/AppHelper/AppHelper.php
+++ master-custom/admin/src/App/AppHelper/AppHelper.php
@@ -4,8 +4,49 @@ namespace src\App\AppHelper;
 abstract class AppHelper {
     protected static $Helper;
 
+    /**
+     * @return mixed
+     * get create instance
+     */
     abstract public static function getInstance();
 
+    /**
+     * @return mixed
+     * get query
+     */
+    abstract public function getQuery();
 
+    /**
+     * @param $key
+     * @return mixed
+     * get query parameter
+     */
+    abstract public function getQueryParam($key);
+
+    /**
+     * @return mixed
+     * get GET all query
+     */
+    abstract public function getGetQuery();
+
+    /**
+     * @param $key
+     * @return mixed
+     * get GET query parameter
+     */
+    abstract public function getGetQueryParam($key);
+
+    /**
+     * @return mixed
+     * get POST all query
+     */
+    abstract public function getPostQuery();
+
+    /**
+     * @param $key
+     * @return mixed
+     * get POST query parameter
+     */
+    abstract public function getPostQueryParam($key);
 }
 ?>
