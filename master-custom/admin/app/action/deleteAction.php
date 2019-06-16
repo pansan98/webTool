a0b544247e1f12190af59f37131106134f62c74d
Pansan98 <edo.fjsjm@icloud.com>
2019-06-16 19:25:20 +0900

    delete action
    Signed-off-by: Pansan98 <edo.fjsjm@icloud.com>

---
+++ master-custom/admin/app/action/deleteAction.php
@@ -0,0 +1,6 @@
+<?php
+    include dirname(__FILE__).'/../../../bootstrap.php';
+    
+    use src\Mod\Controller\Base\BaseController as Controller;
+    
+    $baseController = new Controller();
---
+++ master-custom/admin/src/Mod/Model/Delete/DeleteModel.php
@@ -0,0 +1,12 @@
+<?php
+namespace src\Mod\Model\Delete;
+
+use src\Mod\Model\Base\BaseModel;
+use src\Mod\Model\Session\SessionModel;
+
+class DeleteModel extends BaseModel {
+    
+    protected $_sessionModel;
+    
+    
+}
